ce4d78f5de2a56ea95aa5f19c83d0b516b248300
suyog chari <suygchari@gmail.com>
2025-05-21 15:38:01 +0530

    fix appwrite-config file

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -14,8 +14,7 @@ export const {
 
 const client = new sdk.Client()
 
-client.setEndpoint('https://fra.cloud.appwrite.io/v1').setProject('682c47e20004f0a8f540').setKey('standard_e8f8d9d13389bcc348aa7c1a76fb9cefa9a58068687dd70f1172154772d36e7aff9c9ffa3b6495ff2d690546a37e637a7d2d85f93317ba94df2bd952a132a9e7557eb3ccf6c736899651658e0e67a12ee98ccfeb119fef07e604e8d6d48b64508f05b01570bfc346faf9c9f439e2c545c46de107a3dd8567f921f69a8ca0aada');
-
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client); 
