11074ce51671f280c94a3979960a08e07fa78718
suyog chari <suygchari@gmail.com>
2025-06-03 16:45:00 +0530

    add: success page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -22,3 +22,17 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
     console.error("Error creating appointment:", error);
   }
 };
+
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log('Error fetching appointment:', error);
+  }
+}
