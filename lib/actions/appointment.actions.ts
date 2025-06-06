536743dad63dc5dce76496d2841d27d0d65fc93e
suyog chari <suygchari@gmail.com>
2025-06-06 16:10:16 +0530

    admin page: created admin dashboard with dynamic appointments count

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,15 +1,17 @@
 "use server";
 
-
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
-export const createAppointment = async (appointment: CreateAppointmentParams) => {
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
   try {
     const newAppointment = await databases.createDocument(
       DATABASE_ID!,
@@ -23,7 +25,6 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
   }
 };
 
-
 export const getAppointment = async (appointmentId: string) => {
   try {
     const appointment = await databases.getDocument(
@@ -33,6 +34,45 @@ export const getAppointment = async (appointmentId: string) => {
     );
     return parseStringify(appointment);
   } catch (error) {
-    console.log('Error fetching appointment:', error);
+    console.log("Error fetching appointment:", error);
   }
-}
+};
+
+export const getReccentAppointments = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if (appointment.status === "scheduled") {
+          acc.scheduledCount += 1;
+        } else if (appointment.status === "pending") {
+          acc.pendingCount += 1;
+        } else if (appointment.status === "cancelled") {
+          acc.cancelledCount += 1;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+    return parseStringify(data);
+  } catch (error) {
+    console.error("Error fetching recent appointments:", error);
+  }
+};
