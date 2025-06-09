c9d18822bff2f193fd31672c6d68e61aa1f8e6fc
suyog chari <suygchari@gmail.com>
2025-06-09 12:00:27 +0530

    admin page: added sorting with email and amount, added paggination

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,56 +1,147 @@
-import StatCard from '@/components/StatCard'
-import { getReccentAppointments } from '@/lib/actions/appointment.actions'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { DataTable } from "@/components/table/DataTable";
+import StatCard from "@/components/StatCard";
+import { getReccentAppointments } from "@/lib/actions/appointment.actions";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { columns, Payment } from "@/components/table/columns";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "z@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 80,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 500,
+      status: "pending",
+      email: "m@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "b@example.com",
+    },
+        {
+      id: "728ed52f",
+      amount: 200,
+      status: "pending",
+      email: "a@example.com",
+    },
+  ];
+}
 
 const Admin = async () => {
+  const data = await getData();
 
-  const appointments = await getReccentAppointments()
+  const appointments = await getReccentAppointments();
 
   return (
-    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
-      <header className='admin-header'>
-        <Link href="/" className='cursor-pointer'>
-          <Image 
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
             src="/assets/icons/logo-full.svg"
             height={32}
             width={32}
             alt="WeCare Logo"
-            className='h-8 w-fit'
+            className="h-8 w-fit"
           />
         </Link>
-        <p className='text-16-semibold'>Admin Dashboard</p> 
+        <p className="text-16-semibold">Admin Dashboard</p>
       </header>
-      <main className='admin-main'>
-        <section className='w-full space-y-4'>
-            <h1 className='header'>Welcome 👋</h1>
-            <p className='text-dark-700'>Start the day with managing new appointments</p>
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
         </section>
 
-        <section className='admin-stat'>
-            <StatCard 
-             type="appointments"
-             count={appointments.scheduledCount}
-             label="New Appointments"
-             icon="/assets/icons/appointments.svg"
-            />
-            <StatCard 
-             type="pending"
-             count={appointments.pendingCount}
-             label="Pending Appointments"
-             icon="/assets/icons/pending.svg"
-            />
-            <StatCard 
-             type="cancelled"
-             count={appointments.cancelledCount} 
-             label="Cancelled Appointments"
-             icon="/assets/icons/cancelled.svg"
-            />
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="New Appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending Appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled Appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
         </section>
+
+        <DataTable columns={columns} data={data} />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
